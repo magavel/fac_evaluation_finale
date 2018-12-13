50c4d931af0e65c0f7edb034e68397b40bd4ddb6
Avel Féron Papel <avelferonpapel@MacBook-Pro-de-Avel.local>
2018-12-13 12:21:12 +0100

    creation du devoir

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="icon" href="img/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/mon_site.css">
+</head>
+<body>
+<div class="content">
+
+    <div id="nav">
+        <div id="menuIcone">
+            <img src="logos/EnergiesCollectives_rectangle_Logo_ajuste.svg" alt="">
+        </div>
+        <div id="menuText">
+            <ul>
+                <li><a href="#"> <span class="texteMenu">En savoir plus</span> </a> </li>
+                <li><a href="#"><span class="texteMenu">Nous découvrir</span></a> </li>
+                <li><a href="#"> <span class="texteMenu">Nos installations</span></svg></a> </li>
+                <li><a href="#"><span class="texteMenu">Economisons</span></a> </li>
+                <li><a href="#"> <span class="texteMenu">Contact</span></a> </li>
+
+
+
+            </ul>
+        </div>
+    </div> <!-- fin menu-->
+    <div class="entete"> <img src="Photos/20161202_101925.jpg">
+    </div>
+    <div class="logo">
+        <img src="logos/EnergiesCollectivesLogo_ss_texte.svg">
+        <p><span class="titreLogo">L'energie est notre affaire</span></p>
+        <p><span class="sousTitre">Investissons dans la transition énergétique</span></p>
+    </div>
+
+    <div class="accueil">
+        <div class="picto coop"><IMG src="svg/picto_coop.svg"><p>COOPÉRATEURS</p><p>190</p></div>
+        <div class="picto installation"><img src="svg/picto_photovoltaique.svg"><p>INSTALLATIONS</p><p>8</p></div>
+        <div class="picto production"><img src="svg/picto_production_energie.svg"><p>PRODUCTION ANNUELLE</p><p>141 MWh</p></div>
+        <div class="picto capital"><img src="svg/picto_capital.svg"><p>CAPITAL</p><p>112950 €</p></div>
+
+
+    </div>
+    <div class="presentation point">
+        <div class="articlePresentation">
+            <h2>Produire et mieux consommer de l'électricité renouvellable et local c'est possible</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis labore minus ratione.
+                Ad assumenda beatae consequatur corporis, facere fuga fugiat hic illo illum, incidunt itaque
+                magni qui recusandae suscipit voluptates!
+            </p>
+        </div>
+
+        <div class="articlePresentation point">
+            <h2>Pourquoi devenir sociétaire d'energies revouvellables</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis labore minus ratione.
+                Ad assumenda beatae consequatur corporis, facere fuga fugiat hic illo illum, incidunt itaque
+                magni qui recusandae suscipit voluptates!
+            </p>
+        </div>
+
+
+
+
+    </div>
+
+    <div class="article actualite">
+        <h1>Actualités</h1>
+        <div class="photoArticle"><img src="Photos/IMG_20170121_122143.jpg"></div>
+        <h2>Un 24 novembre de célébration</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!
+        </p>
+
+    </div>
+
+    <div class="article demarche">
+        <h1>Notre démarche</h1>
+        <div class="photoArticle"><img src="Photos/IMG_20170121_122143.jpg"></div>
+        <h2>Notre démarche</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!
+        </p>
+
+    </div>
+
+    <div class="article installation point">
+        <h1>Nos installations</h1>
+        <div class="photoArticle"><img src="Photos/IMG_20170121_122143.jpg"></div>
+        <h2>Atelier Burger, Embrun</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+
+        </p>
+
+    </div>
+
+
+    <div class="article temoignage">
+        <h1>Témoignage</h1>
+        <div class="photoArticle"><img src="Photos/IMG_20170121_122143.jpg"></div>
+        <h2>Mairie de Puy Sanière</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+            Ab architecto debitis deserunt esse facere iusto magnam nam possimus quod repellendus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            In laudantium nostrum obcaecati quaerat quia recusandae tempore, ullam voluptatibus.
+
+        </p>
+
+    </div>
+
+
+    <div class="footer">
+
+        <div>Nous suivre</div>
+        <div>Nous contacter</div>
+        <div>Rechercher</div>
+        <div>Abonnez vous à la Newsletter</div>
+    </div>
+</div>
+
+</body>
+</html>
